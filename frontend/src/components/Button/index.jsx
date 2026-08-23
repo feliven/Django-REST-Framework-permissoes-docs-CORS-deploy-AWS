e6c4e542f1d0035964a809280cf4651af85758d1
FVS-MBA <feliven@gmail.com>
2026-08-23 19:00:53 -0300

    adiciona frontend

---
+++ frontend/src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  color: #dbe4ef;
+  font-size: 18px;
+`;
+
+export default Button;
