a9c051a3e9b7ef0a9bc4495ff34735c016f48e85
FVS-MBA <feliven@gmail.com>
2026-08-23 22:24:02 -0300

    adiciona gunicorn, uvicorn e whitenoise

--- setup/settings.py
+++ setup/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -60,8 +61,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8042",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
 ]
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
 
 ROOT_URLCONF = "setup.urls"
@@ -177,7 +181,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Email
